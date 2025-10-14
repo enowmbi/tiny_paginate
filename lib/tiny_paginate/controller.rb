09039b410e67ad8b5a9a3652e3a466910e388e25
Enow Mbi <benowmbi@yahoo.com>
2025-10-14 19:37:34 +0100

    Add controller

---
+++ lib/tiny_paginate/controller.rb
@@ -0,0 +1,12 @@
+module TinyPaginate
+  class Controller
+    private
+    def set_page_and_extract_portion_from(collection)
+      @page = TinyPaginate::Page.new(page_number: page_number_param, collection: collection)
+    end
+
+    def page_number_param
+      params[:page_number].to_i  >= 0 ? params[:page_number]  :  1
+    end
+  end
+end
