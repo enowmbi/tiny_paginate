4b743bca208b622f5a54d60b57728d213f211269
Enow Mbi <benowmbi@yahoo.com>
2025-10-14 19:38:06 +0100

    Add initializer to include controller methods in host controllers

--- lib/tiny_paginate/railtie.rb
+++ lib/tiny_paginate/railtie.rb
@@ -1,4 +1,9 @@
 module TinyPaginate
   class Railtie < ::Rails::Railtie
+    initializer "tiny_paginate.controller" do
+      ActiveSupport.on_load(:action_controller_base) do
+        ActionController::Base.send :include, TinyPaginate::Controller
+      end
+    end
   end
 end
