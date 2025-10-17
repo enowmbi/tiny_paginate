a2d66dc1dafebd9c508c7b88fe87cc3ddedccebd
Enow Mbi <benowmbi@yahoo.com>
2025-10-17 05:54:23 +0100

    Rename ControllerExtensions to ControllerHelperMethods module

--- lib/tiny_paginate/railtie.rb
+++ lib/tiny_paginate/railtie.rb
@@ -1,20 +1,9 @@
 module TinyPaginate
   class Railtie < ::Rails::Railtie
-    initializer "tiny_paginate.controller" do
+    initializer "tiny_paginate.controller_helper_methods" do
       ActiveSupport.on_load(:action_controller_base) do
-        ActionController::Base.send :include, TinyPaginate::ControllerExtension
+        include TinyPaginate::ControllerHelperMethods
       end
     end
-
-    # if defined?(FactoryBotRails)
-    #   config.factory_bot.definition_file_paths +=
-    #     [ File.expand_path("spec/factories", __dir__) ]
-    # end
-
-    # config.generators do |g|
-    #   g.test_framework :rspec
-    #   g.fixture_replacement :factory_bot
-    #   g.factory_bot dir: "spec/factories"
-    # end
   end
 end
